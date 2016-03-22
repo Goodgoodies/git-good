cf9396ff9946330a8c9b64d0ee3698da16590048
Spencer Pollock <srepollock@gmail.com>
2016-03-21 23:14:02 -0700

    :confused: Fixed? I think?

--- update/git_good_updates.sh
+++ update/git_good_updates.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Setup an alias for the update
-git config --global alias.update '!git remote update -p; git merge -ff-only @{u}'
+git config --global alias.update '!git remote update -p; git merge --ff-only @{u}'
 # Echo into a new bash: "move into ~/.gitgood and update from master. Close when done."
 bash --rcfile <(echo "cd ~/.gitgood && git checkout master && git update && exit")
